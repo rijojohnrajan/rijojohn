d410a2c789b523da501c456d05dbe1bc4dc5374f
Rijo <32301048+rijojohnrajan@users.noreply.github.com>
2018-01-18 12:10:35 +0530

    Update 1.rb

--- 1.rb
+++ 1.rb
@@ -2,5 +2,21 @@ trial=[14,15,16]
  puts trail
 trial.max
  trail.sort 
- 
+ puts 'Give me a number'
+number = gets.chomp
+puts number.to_i
+output_number = number.to_i + 1
 
+class Time
+  def yesterday
+    self - 86400
+  end
+end
+
+a[2]           
+a.[](2)   
+a.reverse   
+
+do
+  puts 'Hello, World!'
+end
